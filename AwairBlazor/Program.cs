16d75735388b81ba11c415b7c3922ac2c5c67a34
Stephen Klancher <smknight@gmail.com>
2020-09-26 14:33:32 -0700

    Resolves #8. Created new project from latest template and then migrated previous code over.

--- AwairBlazor/Program.cs
+++ AwairBlazor/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using AwairBlazor.Services;
 using Blazored.LocalStorage;
-using AwairBlazor.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AwairBlazor
 {
@@ -15,15 +17,13 @@ namespace AwairBlazor
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddAppData();
 
-            //builder.Services.AddSingleton<Services.AppData>();
             await builder.Build().RunAsync();
-			
-			//test change
         }
     }
 }
